900d42f98aae29c0359556734b9e7c89c4e5654a
Jan Muehlemann <jan.muehlemann@gmail.com>
2012-07-13 13:20:08 +0200

    pre version, more options to handle stuff in denormalizer, onHandle call added

--- Jakefile.js
+++ Jakefile.js
@@ -8,7 +8,7 @@ task('build', [], function(debug) {
     
     // a basic smoosh configuration object
     smoosh.config({
-      "VERSION": "0.5.0",
+      "VERSION": "0.5.5_pre",
       "JAVASCRIPT": {
         "DIST_DIR": "./",
         "backboneCQRS": [
