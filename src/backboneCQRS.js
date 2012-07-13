2555a35ce240c3371cdf10f911b3ef0f4179eb87
Jan Mühlemann <jan.muehlemann@gmail.com>
2012-07-13 15:58:21 +0300

    first resolve denorms than observers

--- src/backboneCQRS.js
+++ src/backboneCQRS.js
@@ -249,19 +249,19 @@
 
         // handles incoming events from hub
         handle: function(evt) {
-            // to observing commands
-            var pending = this.getPendingCommand(evt);
-            if (pending) {
-                pending.callback(evt);
-                this.removePendingCommand(pending);
-            }
-
             // to denormalizers
             var denorm = this.getDenormalizer(evt.name);
 
             _(denorm).each(function(d) {
                 d.handle(evt);
             });
+            
+            // to observing commands
+            var pending = this.getPendingCommand(evt);
+            if (pending) {
+                pending.callback(evt);
+                this.removePendingCommand(pending);
+            }
         },
 
         // a model can bind to events via ev = 'modelName:id' 
